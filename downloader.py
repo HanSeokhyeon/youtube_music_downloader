031da6d9048d70219742c73c3c07cc7a51b18ef7
hanseokhyeon <sah0322@naver.com>
2020-11-04 14:49:33 +0900

    make recursive for error file & audio normarlize

--- downloader.py
+++ downloader.py
@@ -5,6 +5,7 @@ youtube music downloader
 import numpy as np
 import argparse
 import os
+from pydub import AudioSegment, effects
 
 parser = argparse.ArgumentParser(description='download youtube music.')
 parser.add_argument('link_path', type=str, help='file path of links')
@@ -17,13 +18,25 @@ except IOError:
     print("no such {}".format(args.link_path))
     exit(-1)
 
-for i, (singer, song, link) in enumerate(links[142:]):
-    command = "youtube-dl -x --audio-format mp3 --audio-quality 0 {}".format(link)
-    try:
-        os.system(command)
-    except:
-        continue
-    for filename in os.listdir('./'):
-        if filename.endswith('.mp3'):
-            os.rename('./'+filename, './output/{} - {}.mp3'.format(singer, song))
-    print("{} {} - {} complete".format(i+1, singer, song))
+
+def download_music(infos):
+    errors = []
+    for i, (singer, song, link) in enumerate(infos):
+        command = "youtube-dl -x --audio-format mp3 --audio-quality 0 {}".format(link)
+        return_value = os.system(command)
+        if return_value != 0:
+            errors.append((singer, song, link))
+            continue
+
+        for filename in os.listdir('./'):
+            if filename.endswith('.mp3'):
+                raw = AudioSegment.from_mp3(filename)
+                raw_normalized = effects.normalize(raw)
+                raw_normalized.export(filename, format="mp3")
+                os.rename('./' + filename, './output/{} - {}.mp3'.format(singer, song))
+        print("{} {} - {} complete".format(i + 1, singer, song))
+    if errors:
+        download_music(errors)
+
+
+download_music(links[1:])
